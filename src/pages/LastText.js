0b826b14fbc10ba524024ed48c7cdbdb26d6371e
tima001 <tbahytzanuly@gmail.com>
2025-03-04 09:28:38 +0500

    For Dimash

--- src/pages/LastText.js
+++ src/pages/LastText.js
@@ -1,6 +1,5 @@
 import React from "react";
 import styled from "@emotion/styled";
-import flower from "../img/flower.png";
 
 const LastText = () => {
   return (
@@ -9,7 +8,7 @@ const LastText = () => {
             Будут сюрпризы, улыбки и, конечно же, я – твой главный поклонник! 🎭<br/><br/>
 
             ⚠ Внимание: без тебя это приглашение превращается в тыкву! 🎃<br/><br/>
-            Так что приходи, ведь идеальное 14 февраля – это когда ты рядом.
+            Так что приходи, ведь идеальное время – это когда ты рядом.
         </TitleText>
     </TextContentWrapper>
   );
@@ -25,34 +24,14 @@ const TextContentWrapper = styled.div`
   gap: 12px;
 `;
 
-const NameText = styled.div`
-  font-family: "GreatFont";
-  color: #a11b14;
-  font-size: 42px;
-  line-height: 280%;
-  @media (max-width: 320px) {
-    font-size: 38px;
-  }
-`;
+
 
 const TitleText = styled.div`
   font-family: "BKANTKZ", Arial, sans-serif;
   color: #a11b14;
   font-size: 19px;
   text-align: start;
-  font-weight: 600;
   font-weight: 200;
     padding: 0 20px;
 `;
-const ContentText = styled.h2`
-  font-family: "BKANTKZ", Arial, sans-serif;
-  line-height: 160%;
-  font-weight: 200;
-  text-align: center;
-  font-size: 18px;
-  margin: 0;
 
-  @media (max-width: 320px) {
-    font-size: 18px;
-  }
-`;
